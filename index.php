228414d747bc0f82cf91646118e7082296afd0fa
and96437345 <and96437345@yandex.ru>
2024-02-19 00:55:21 +0300

    test_20_half

--- index.php
+++ index.php
@@ -867,3 +867,71 @@ function likes( $names ) {
 //   }
 // };
 
+function number2words(int $n): string {
+  $res = 'zero';
+  $array_num = [
+    0 => 'zero',
+    1 => 'one',
+    2 => 'two',
+    3 => 'three',
+    4 => 'four',
+    5 => 'five',
+    6 => 'six',
+    7 => 'seven',
+    8 => 'eight',
+    9 => 'nine',
+    10 => 'ten',
+    11 => 'eleven',
+    12 => 'twelve',
+    13 => 'thirteen',
+    14 => 'fourteen',
+    15 => 'fifteen',
+    16 => 'sixteen',
+    17 => 'seventeen',
+    18 => 'eighteen',
+    19 => 'nineteen',
+    20 => 'twenty',
+    30 => 'thirty',
+    40 => 'forty',
+    50 => 'fifty',
+    60 => 'sixty',
+    70 => 'seventy',
+    80 => 'eighty',
+    90 => 'ninety'
+  ];
+  if ($n > 0 && $n <= 20){
+    $res = $array_num[$n];
+  }
+  if ($n > 20 && $n < 100) {
+    $low = $n % 10;
+    $mid = $n % 100 - $low;
+    if ($low == 0) {
+      $res = $array_num[$mid];
+    } else {
+      $res = $array_num[$mid].'-'.$array_num[$low];
+    }
+  }
+  if ($n >= 100 && $n <= 1000) {
+    $mid = $n % 100;
+    $hi = ($n % 1000 - $mid) / 100;
+    if ($mid <= 20) {
+      if ($mid == 0) {
+        $res = $array_num[$hi].' hundred';
+      } else {
+        $res = $array_num[$hi].' hundred '.$array_num[$mid];
+      }
+    } else {
+      $low = $mid % 10;
+      $mid = $mid - $low;
+      if ($low == 0) {
+        $res = $array_num[$hi].' hundred '.$array_num[$mid];
+      } else {
+        $res = $array_num[$hi].' hundred '.$array_num[$mid].'-'.$array_num[$low];
+      }
+    }
+  }
+  return $res;
+}
+
+$a = readline('Введите чсло: ');
+echo number2words($a);
