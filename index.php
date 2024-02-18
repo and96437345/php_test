f198157ee8e632889f0d9ecef82f3e862317f5b9
and96437345 <and96437345@yandex.ru>
2024-02-18 17:13:19 +0300

    test_18
    PHP OOP

--- index.php
+++ index.php
@@ -466,120 +466,121 @@ function likes( $names ) {
 
 // likes(["Alex", "Jacob", "Mark", "Max"]);
 
-class Person {
-  const species = 'Homo Sapiens';
-  public $name;
-  public $age;
-  public $occupation;
-  public function __construct($name, $age, $occupation){
-    $this->name = $name;
-    $this->age = $age;
-    $this->occupation = $occupation;
-  }
-  public function introduce() {
-    return "Hello, my name is $this->name";
-  }
-  public function describe_job() {
-    return "I am currently working as a(n) $this->occupation";
-  }
-  static function greet_extraterrestrials($species) {
-    return "Welcome to Planet Earth $species!";
-  }
-}
+// class Person {
+//   const species = 'Homo Sapiens';
+//   public $name;
+//   public $age;
+//   public $occupation;
+//   public function __construct($name, $age, $occupation){
+//     $this->name = $name;
+//     $this->age = $age;
+//     $this->occupation = $occupation;
+//   }
+//   public function introduce() {
+//     return "Hello, my name is $this->name";
+//   }
+//   public function describe_job() {
+//     return "I am currently working as a(n) $this->occupation";
+//   }
+//   static function greet_extraterrestrials($species) {
+//     return "Welcome to Planet Earth $species!";
+//   }
+// }
 
 // $humanoid = new Person('Humanoid','37','Wega');
 // print $humanoid->introduce();
 // print $humanoid->describe_job();
 // print $humanoid->greet_extraterrestrials('Martians');
 
-class Salesman extends Person {
-  public function __construct($name, $age) {
-    $this->name = $name;
-    $this->age = $age;
-    $this->occupation = 'Salesman';
-  }
-  public function introduce() {
-    return "Hello, my name is $this->name, don't forget to check out my products!";
-  }
-}
-class ComputerProgrammer extends Person {  
-  public function __construct($name, $age) {
-    $this->name = $name;
-    $this->age = $age;
-    $this->occupation = 'Computer Programmer';
-  }
-  public function describe_job() {
-    return "I am currently working as a(n) $this->occupation, don't forget to check out my Codewars account ;)";
-  }
-}
-class WebDeveloper extends ComputerProgrammer {
-  public function __construct($name, $age) {
-    $this->name = $name;
-    $this->age = $age;
-    $this->occupation = 'Web Developer';
-  }
-  public function describe_job() {
-    return parent::describe_job()." And don't forget to check on my website :D";
-  }
-  public function describe_website () {
-    return "My professional world-class website is made from HTML, CSS, Javascript and PHP!";
-  }
-}
+// class Salesman extends Person {
+//   public function __construct($name, $age) {
+//     $this->name = $name;
+//     $this->age = $age;
+//     $this->occupation = 'Salesman';
+//   }
+//   public function introduce() {
+//     return "Hello, my name is $this->name, don't forget to check out my products!";
+//   }
+// }
+// class ComputerProgrammer extends Person {  
+//   public function __construct($name, $age) {
+//     $this->name = $name;
+//     $this->age = $age;
+//     $this->occupation = 'Computer Programmer';
+//   }
+//   public function describe_job() {
+//     return "I am currently working as a(n) $this->occupation, don't forget to check out my Codewars account ;)";
+//   }
+// }
+// class WebDeveloper extends ComputerProgrammer {
+//   public function __construct($name, $age) {
+//     $this->name = $name;
+//     $this->age = $age;
+//     $this->occupation = 'Web Developer';
+//   }
+//   public function describe_job() {
+//     return parent::describe_job()." And don't forget to check on my website :D";
+//   }
+//   public function describe_website () {
+//     return "My professional world-class website is made from HTML, CSS, Javascript and PHP!";
+//   }
+// }
+
 // $obj = new WebDeveloper('Stiven','40');
 // echo $obj->describe_job();
 
-class PersonNew {
-  protected $name;
-  protected $age;
-  protected $occupation;
-  public function __construct($name, $age, $occupation){
-    if (!is_string($name)) {
-      throw new InvalidArgumentException('Name must be a string!');
-    } else {
-      $this->name = $name;
-    }
-    if (!is_int($age) || $age < 0) {
-      throw new InvalidArgumentException('Age must be a non-negative integer!');
-    } else {
-      $this->age = $age;
-    }
-    if (!is_string($occupation)) {
-      throw new InvalidArgumentException('Occupation must be a string!');
-    } else {
-      $this->occupation = $occupation;
-    }
-  }
-  public function set_name($name) {
-    if (!is_string($name)) {
-      throw new InvalidArgumentException('Name must be a string!');
-    } else {
-      $this->name = $name;
-    }
-  }
-  public function set_age($age) {
-    if (!is_int($age) || $age < 0) {
-      throw new InvalidArgumentException('Age must be a non-negative integer!');
-    } else {
-      $this->age = $age;
-    }
-  }
-  public function set_occupation($occupation) {
-    if (!is_string($occupation)) {
-      throw new InvalidArgumentException('Occupation must be a string!');
-    } else {
-      $this->occupation = $occupation;
-    }
-  }
-  public function get_name() {
-    return $this->name;
-  }
-  public function get_age() {
-    return $this->age;
-  }
-  public function get_occupation() {
-    return $this->occupation;
-  }
-}
+// class PersonNew {
+//   protected $name;
+//   protected $age;
+//   protected $occupation;
+//   public function __construct($name, $age, $occupation){
+//     if (!is_string($name)) {
+//       throw new InvalidArgumentException('Name must be a string!');
+//     } else {
+//       $this->name = $name;
+//     }
+//     if (!is_int($age) || $age < 0) {
+//       throw new InvalidArgumentException('Age must be a non-negative integer!');
+//     } else {
+//       $this->age = $age;
+//     }
+//     if (!is_string($occupation)) {
+//       throw new InvalidArgumentException('Occupation must be a string!');
+//     } else {
+//       $this->occupation = $occupation;
+//     }
+//   }
+//   public function set_name($name) {
+//     if (!is_string($name)) {
+//       throw new InvalidArgumentException('Name must be a string!');
+//     } else {
+//       $this->name = $name;
+//     }
+//   }
+//   public function set_age($age) {
+//     if (!is_int($age) || $age < 0) {
+//       throw new InvalidArgumentException('Age must be a non-negative integer!');
+//     } else {
+//       $this->age = $age;
+//     }
+//   }
+//   public function set_occupation($occupation) {
+//     if (!is_string($occupation)) {
+//       throw new InvalidArgumentException('Occupation must be a string!');
+//     } else {
+//       $this->occupation = $occupation;
+//     }
+//   }
+//   public function get_name() {
+//     return $this->name;
+//   }
+//   public function get_age() {
+//     return $this->age;
+//   }
+//   public function get_occupation() {
+//     return $this->occupation;
+//   }
+// }
 
 // $obj = new PersonNew('Stiven', 32, 'Programmer');
 // $obj->set_name(true);
@@ -631,3 +632,238 @@ class PersonNew {
 //   }
 
 // }
+
+// class Person {
+//   const species = 'Homo Sapiens';
+//   public $name;
+//   public $age;
+//   public $occupation;
+//   public function __construct($name, $age, $occupation){
+//     $this->name = $name;
+//     $this->age = $age;
+//     $this->occupation = $occupation;
+//   }
+//   public function introduce() {
+//     return "Hello, my name is $this->name";
+//   }
+//   final public function describe_job() {
+//     return "I am currently working as a(n) $this->occupation";
+//   }
+//   final static function greet_extraterrestrials($species) {
+//     return "Welcome to Planet Earth $species!";
+//   }
+// }
+
+// class ComputerProgrammer extends Person {
+//   public function introduce() {
+//     return "Hello, my name is $this->name and I am a $this->occupation";
+//   }
+// }
+// $obj = new ComputerProgrammer('Stiven', 36, 'Programmer');
+// echo $obj->introduce();
+
+// interface CanFly {
+//   public function fly();
+// }
+// interface CanSwim {
+//   public function swim();
+// }
+// interface CanClimb {
+//   public function climb();
+// }
+// interface CanGreet {
+//   public function greet($name);
+// }
+// interface CanIntroduce {
+//   public function speak();
+//   public function introduce();
+// }
+// interface CanSpeak {
+//   public function speak();
+// }
+// class Bird implements Canfly {
+//   public $name;
+//   public function __construct($name) {
+//     $this->name = $name;
+//   }
+//   public function fly() {
+//     return 'I am flying';
+//   }
+//   public function chirp() {
+//     return 'Chirp chirp';
+//   }
+// }
+// class Duck extends Bird implements CanSwim {
+//   public function chirp() {
+//     return 'Quack quack';
+//   }
+//   public function swim() {
+//     return "Splash! I'm swimming";
+//   }
+// }
+// class Cat implements CanClimb{
+//   public function climb() {
+//     return "Look, I'm climbing a tree";
+//   }
+//   public function meow() {
+//     return "Meow meow";
+//   }
+//   public function play($name) {
+//     return "Hey $name, let's play!";
+//   }
+// }
+// class Dog implements CanSwim, CanGreet {
+//   public function swim() {
+//     return "I'm swimming, woof woof";
+//   }
+//   public function greet($name) {
+//     return "Hello $name, welcome to my home";
+//   }
+//   public function bark() {
+//     return "Woof woof";
+//   }
+// }
+// class Person implements CanGreet, CanIntroduce {
+//   public $name;
+//   public $age;
+//   public $occupation;
+//   public function __construct($name, $age, $occupation) {
+//     $this->name = $name;
+//     $this->age = $age;
+//     $this->occupation = $occupation;
+//   }
+//   public function greet($name) {
+//     return "Hello $name, how are you?";
+//   }
+//   public function speak() {
+//     return "What am I supposed to say again?";
+//   }
+//   public function introduce() {
+//     return "Hello, my name is $this->name, I am $this->age years old and I am currently working as a(n) $this->occupation";
+//   }
+// }
+
+// $obj = new Cat('Cat');
+// echo $obj->play('reew');
+
+
+
+// abstract class Person {
+//   public $name;
+//   public $age;
+//   public $gender;
+//   public function __construct($name, $age, $gender) {
+//     $this->name = $name;
+//     $this->age = $age;
+//     $this->gender = $gender;
+//   }
+//   abstract public function introduce();
+//   public function greet($name) {
+//     return "Hello $name";
+//   }
+// }
+
+// final class Child extends Person {
+//   public $aspirations = [];
+//   public function __construct($name, $age, $gender, $aspirations) {
+//     $this->name = $name;
+//     $this->age = $age;
+//     $this->gender = $gender;
+//     $this->aspirations = $aspirations;
+//   }
+//   public function introduce() {
+//     return "Hi, I'm $this->name and I am $this->age years old";
+//   }
+//   public function greet($name) {
+//     return "Hi $name, let's play!";
+//   }
+//   public function say_dreams() {
+//     return "I would like to be a(n) ".say_list($this->aspirations)." when I grow up."; 
+//   }
+// }
+
+// class ComputerProgrammer extends Person {
+//   public $occupation = 'Computer Programmer';
+//   public function introduce() {
+//     return "Hello, my name is $this->name, I am $this->age years old and I am a(n) $this->occupation";
+//   }
+//   public function greet($name) {
+//     return "Hello $name, I'm $this->name, nice to meet you";
+//   }
+//   public function advertise() {
+//     return "Don't forget to check out my coding projects";
+//   }
+// }
+
+// $obj = new ComputerProgrammer('Donald', 3, 'male');
+// echo $obj->introduce();
+
+// $object_oriented_php = new class {
+//   public $description = 'An amazing PHP Kata Series, complete with 10 top-quality Kata containing a large number of both fixed and random tests, that teaches both the fundamentals of object-oriented programming in PHP (in the first 7 Kata of this Series) and more advanced OOP topics in PHP (in the last 3 Kata of this Series) such as interfaces, abstract classes and even anonymous classes in a way that stimulates critical thinking and encourages independent research';
+//   public $kata_list = [];
+//   public $kata_count = 10;
+// };
+// $str = serialize($object_oriented_php);
+// echo $str;
+
+// $author = new class('Donald', 17, 'Male', 'Computer programmer') {
+//   public $name, $age, $gender, $occupation;
+//   public function __construct($name, $age, $gender, $occupation) {
+//     $this->name = $name;
+//     $this->age = $age;
+//     $this->gender = $gender;
+//     $this->occupation = $occupation;
+//   }
+//   public function say() {
+//     return "$this->name, aged $this->age, who is a $this->occupation proficient in Javascript and PHP and is a PHP enthusiast";
+//   }
+// };
+// echo $object_oriented_php;
+
+
+
+// $object_oriented_php = new class() {
+//   public function __construct() {
+//       $this->author = new class() {
+//           public $name = 'Donald';
+//           public $age = '17';
+//           public $gender = 'Male';
+//           public $occupation = 'Computer Programmer';
+//           public function __toString() {
+//               return 'Donald, aged 17, who is a computer programmer proficient in Javascript and PHP and is a PHP enthusiast';
+//           }
+//       };
+//   }
+//   public $description = "An amazing PHP Kata Series, complete with 10 top-quality Kata containing a large number of both fixed and random tests, that teaches both the fundamentals of object-oriented programming in PHP (in the first 7 Kata of this Series) and more advanced OOP topics in PHP (in the last 3 Kata of this Series) such as interfaces, abstract classes and even anonymous classes in a way that stimulates critical thinking and encourages independent research";
+//   public $kata_list = [
+//       'Object-Oriented PHP #1 - Classes, Public Properties and Methods',
+//       'Object-Oriented PHP #2 - Class Constructors and $this',
+//       'Object-Oriented PHP #3 - Class Constants and Static Methods',
+//       'Object-Oriented PHP #4 - People, people, people (Practice)',
+//       'Object-Oriented PHP #5 - Classical Inheritance',
+//       'Object-Oriented PHP #6 - Visibility',
+//       'Object-Oriented PHP #7 - The "Final" Keyword',
+//       'Object-Oriented PHP #8 - Interfaces [Advanced]',
+//       'Object-Oriented PHP #9 - Abstract Classes [Advanced]',
+//       'Object-Oriented PHP #10 - Objects on the Fly [Advanced]'];
+//   public $kata_count = 10;
+//   public $author;
+//   public function advertise($name) {
+//       return "Hey " . $name . ", don't forget to check out this great PHP Kata Series authored by Donald called \"Object-Oriented PHP\"";
+//   }
+//   public function get_kata_by_number($num) {
+// echo $num. " ";
+//       if ($num > 10 || $num < 1 || !is_int($num)) {
+//           throw new InvalidArgumentException();
+//       } else {
+//           return $this->kata_list[$num - 1];
+//       }
+//   }
+//   public function complete() {
+//       return "Hooray, I've finally completed the entire \"Object-Oriented PHP\" Kata Series!!!";
+//   }
+//   public function __toString() {
+//       return $this->description;
+//   }
+// };
+
